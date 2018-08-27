48a1e2cf36bab5db0b68ff01e868e59c6cb2cd71
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-27 17:22:15 -0600

    first go with vue

--- src/store.js
+++ src/store.js
@@ -5,12 +5,91 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    machine: {
+      name: "vendr",
+      items: [{
+        id: 1,
+        imgUrl: "https://t3.rbxcdn.com/35daf5e6f29452133a28dd35cc7eaf00",
+        quantity: 8,
+        price: 2.00,
+        class: "item1"
+      },
+      {
+        id: 2,
+        imgUrl: "https://cdn.shopify.com/s/files/1/0996/7396/products/cheetos.png?v=1461538280",
+        quantity: 7,
+        price: 2.25,
+        class: "item2"
+      },
+      {
+        id: 3,
+        imgUrl: "https://i5.walmartimages.com/asr/189b626c-796b-4f32-b5fd-d8a5d91936b8_1.a97eebc9db676df94353b96ad88e5c92.jpeg?odnHeight=560&odnWidth=560&odnBg=FFFFFF",
+        quantity: 8,
+        price: 3.00,
+        class: "item3"
+      }],
+      curTotal: 0.00
+    },
+    currencies: [{
+      id: 1,
+      name: "nickel",
+      value: 0.05
+    },
+    {
+      id: 2,
+      name: "dime",
+      value: 0.10
+    },
+    {
+      id: 3,
+      name: "quarter",
+      value: 0.25
+    },
+    {
+      id: 4,
+      name: "dollar",
+      value: 1.00
+    }]
   },
   mutations: {
-
+    setTotal(state, newTotal){
+      state.machine.curTotal = newTotal
+    },
+    makePurchase(state, obj){
+      console.log(obj)
+      let item = state.machine.items.find(i => i.id == obj.id)
+      item.quantity = obj.quantity
+      state.machine.curTotal = obj.total
+    },
+    addClass(state, obj){
+      console.log(obj)
+      let item = state.machine.items.find(i => i.id == obj.id)
+      item.class += " greyed-out"
+    }
   },
   actions: {
-
+    purchase({ dispatch, commit, state }, payload) {
+      let item = state.machine.items.find(i => i.id == payload)
+      if(state.machine.curTotal >= item.price){
+        let newQuantity = item.quantity - 1
+        let newTotal = state.machine.curTotal - item.price
+        commit("makePurchase", {
+          id: payload,
+          quantity: newQuantity,
+          total: newTotal
+        })
+      }
+      if(item.quantity <= 0){
+        let classStr = "greyed-out"
+        commit('addClass', {
+          id: payload,
+          classStr: "greyed-out"
+        })
+      }
+    },
+    addMoney({dispatch, commit, state}, payload){
+      let newTotal = state.machine.curTotal + payload
+      commit('setTotal', newTotal)
+    }
   }
 })
---
+++ src/views/Vendr.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="vendr vend-img">
+    <h1>{{machine.name}}</h1>
+    <div class="vendr-items">
+            <a type="button"><img 
+    v-for="obj in machine.items"
+    :key="obj.id"
+    :src="obj.imgUrl"
+    :class="obj.class"
+    @click="purchase(obj.id)"
+    ></a>  
+    </div>
+    <div class="money-display">{{parseFloat(machine.curTotal).toFixed(2)}}</div>
+    <div class="currency-btns">
+      <button
+      v-for="obj in currencies"
+      :key ="obj.id"
+      @click="addMoney(obj.value)"
+      >{{obj.name}}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    machine(){
+      return this.$store.state.machine
+    },
+    currencies(){
+      return this.$store.state.currencies
+    },
+  },
+  methods: {
+    purchase(id){
+      this.$store.dispatch("purchase", id)
+    },
+    addMoney(value) {
+      this.$store.dispatch("addMoney", value)
+    },
+  }
+}
+</script>
+
+<style>
+/* * {
+  outline: 1px solid red;
+} */
+.vend-img {
+  background-image: url('https://media.istockphoto.com/vectors/vending-machine-of-food-and-beverage-automatic-selling-vector-vector-id901409080?k=6&m=901409080&s=612x612&w=0&h=eQZaAvlDdxk09pz2UlaCjcq8ygYCGoG4zRwo2fql2Fg=');
+  background-size: contain;
+  height: 80vh;
+  background-repeat: no-repeat;
+  margin: auto;
+  width: 80vh;
+  position: relative;
+}
+.vendr-items {
+  position: absolute;
+  bottom: 25vh;
+  left: 24vh;
+  position: absolute;
+  height: 40.5vh;
+  width: 22.5vh;
+}
+.money-display {
+  height: 3.1vh;
+  width: 6.7vh;
+  position: absolute;
+  left: 50vh;
+  bottom: 55.3vh;
+  color: green;
+  font-size: 1.5rem;
+}
+.item1 {
+  height: 5.5vh;
+  width: 6vh;
+  position: absolute;
+  left: 8vh;
+  bottom: 3vh;
+  cursor: pointer;
+}
+.item2 {
+  height: 4vh;
+  width: 5vh;
+  position: absolute;
+  left: 8.7vh;
+  bottom: 18.3vh;
+  cursor: pointer;
+}
+.item3 {
+  height: 4vh;
+  width: 5vh;
+  position: absolute;
+  left: .1vh;
+  bottom: 32.5vh;
+  cursor: pointer;
+}
+.greyed-out{
+  opacity: 0.2;
+}
+</style>
+
+
