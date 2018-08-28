2de7a09674a7d280268ba168fea56a7318d849f1
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-27 21:51:11 -0600

    money functions

--- src/store.js
+++ src/store.js
@@ -10,61 +10,82 @@ export default new Vuex.Store({
       items: [{
         id: 1,
         imgUrl: "https://t3.rbxcdn.com/35daf5e6f29452133a28dd35cc7eaf00",
-        quantity: 8,
+        quantity: 3,
         price: 2.00,
-        class: "item1"
+        class: "item1",
+        disabled: false
       },
       {
         id: 2,
         imgUrl: "https://cdn.shopify.com/s/files/1/0996/7396/products/cheetos.png?v=1461538280",
-        quantity: 7,
+        quantity: 4,
         price: 2.25,
-        class: "item2"
+        class: "item2",
+        disabled: false
       },
       {
         id: 3,
         imgUrl: "https://i5.walmartimages.com/asr/189b626c-796b-4f32-b5fd-d8a5d91936b8_1.a97eebc9db676df94353b96ad88e5c92.jpeg?odnHeight=560&odnWidth=560&odnBg=FFFFFF",
-        quantity: 8,
+        quantity: 2,
         price: 3.00,
-        class: "item3"
+        class: "item3",
+        disabled: false
       }],
-      curTotal: 0.00
+      curTotal: 0.00,
     },
-    currencies: [{
-      id: 1,
-      name: "nickel",
-      value: 0.05
-    },
-    {
-      id: 2,
-      name: "dime",
-      value: 0.10
-    },
-    {
-      id: 3,
-      name: "quarter",
-      value: 0.25
-    },
-    {
-      id: 4,
-      name: "dollar",
-      value: 1.00
-    }]
+    wallet: {
+      moneySum: 15.65,
+      currencies: [{
+        id: 1,
+        name: "nickel",
+        value: 0.05,
+        disabled: false
+      },
+      {
+        id: 2,
+        name: "dime",
+        value: 0.10,
+        disabled: false
+      },
+      {
+        id: 3,
+        name: "quarter",
+        value: 0.25,
+        disabled: false
+      },
+      {
+        id: 4,
+        name: "dollar",
+        value: 1.00,
+        disabled: false
+      }]
+    }
   },
   mutations: {
     setTotal(state, newTotal){
       state.machine.curTotal = newTotal
     },
     makePurchase(state, obj){
-      console.log(obj)
       let item = state.machine.items.find(i => i.id == obj.id)
       item.quantity = obj.quantity
       state.machine.curTotal = obj.total
     },
     addClass(state, obj){
-      console.log(obj)
       let item = state.machine.items.find(i => i.id == obj.id)
       item.class += " greyed-out"
+      item.disabled = true
+      console.log(item)
+    },
+    minusWallet(state, newTotal){
+      state.wallet.moneySum = newTotal
+    },
+    disableMoney(state, id){
+      let btn = state.wallet.currencies.find(c => c.id == id)
+      btn.disabled = true
+    },
+    changeBack(state, sum){
+      state.machine.curTotal = 0.00
+      state.wallet.moneySum = sum
     }
   },
   actions: {
@@ -90,6 +111,20 @@ export default new Vuex.Store({
     addMoney({dispatch, commit, state}, payload){
       let newTotal = state.machine.curTotal + payload
       commit('setTotal', newTotal)
+    },
+    minusWallet({dispatch, commit, state}, payload){
+      let newTotal = state.wallet.moneySum - payload
+      commit('minusWallet', newTotal)
+    },
+    enoughMoney({dispatch, commit, state}, value){
+      let btn = state.wallet.currencies.find(c => c.value == value)
+      if(state.wallet.moneySum < btn.value){
+        commit('disableMoney', btn.id)
+      }
+    },
+    getChange({dispatch, commit, state}){
+      let walletSum = state.wallet.moneySum + state.machine.curTotal
+      commit("changeBack", walletSum)
     }
   }
 })
--- src/views/Vendr.vue
+++ src/views/Vendr.vue
@@ -2,20 +2,28 @@
   <div class="vendr vend-img">
     <h1>{{machine.name}}</h1>
     <div class="vendr-items">
-            <a type="button"><img 
-    v-for="obj in machine.items"
-    :key="obj.id"
-    :src="obj.imgUrl"
-    :class="obj.class"
-    @click="purchase(obj.id)"
-    ></a>  
+        <button
+          v-for="obj in machine.items"
+          :key="obj.id"
+          @click="purchase(obj.id)"
+          :disabled="obj.disabled"
+        ><img
+          :src="obj.imgUrl"
+          :class="obj.class"
+      ></button>
     </div>
     <div class="money-display">{{parseFloat(machine.curTotal).toFixed(2)}}</div>
+    <button 
+      :class="{getChange: hasMoney, gone: !hasMoney}"
+      @click="getChange()"
+    ></button>
     <div class="currency-btns">
+      <div class="wallet">${{parseFloat(wallet.moneySum).toFixed(2)}}</div>
       <button
-      v-for="obj in currencies"
+      v-for="obj in wallet.currencies"
       :key ="obj.id"
-      @click="addMoney(obj.value)"
+      @click="moveMoney(obj.value)"
+      :disabled="obj.disabled"
       >{{obj.name}}</button>
     </div>
   </div>
@@ -24,22 +32,30 @@
 <script>
 export default {
   computed: {
-    machine(){
-      return this.$store.state.machine
+    machine() {
+      return this.$store.state.machine;
     },
-    currencies(){
-      return this.$store.state.currencies
+    wallet() {
+      return this.$store.state.wallet;
     },
+    hasMoney() {
+      return this.$store.state.machine.curTotal > 0;
+    }
   },
   methods: {
-    purchase(id){
-      this.$store.dispatch("purchase", id)
+    purchase(id) {
+      this.$store.dispatch("purchase", id);
     },
-    addMoney(value) {
-      this.$store.dispatch("addMoney", value)
+    moveMoney(value) {
+      this.$store.dispatch("addMoney", value);
+      this.$store.dispatch("minusWallet", value);
+      this.$store.dispatch("enoughMoney", value);
     },
+    getChange() {
+      this.$store.dispatch("getChange");
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -47,7 +63,7 @@ export default {
   outline: 1px solid red;
 } */
 .vend-img {
-  background-image: url('https://media.istockphoto.com/vectors/vending-machine-of-food-and-beverage-automatic-selling-vector-vector-id901409080?k=6&m=901409080&s=612x612&w=0&h=eQZaAvlDdxk09pz2UlaCjcq8ygYCGoG4zRwo2fql2Fg=');
+  background-image: url("https://media.istockphoto.com/vectors/vending-machine-of-food-and-beverage-automatic-selling-vector-vector-id901409080?k=6&m=901409080&s=612x612&w=0&h=eQZaAvlDdxk09pz2UlaCjcq8ygYCGoG4zRwo2fql2Fg=");
   background-size: contain;
   height: 80vh;
   background-repeat: no-repeat;
@@ -70,7 +86,7 @@ export default {
   left: 50vh;
   bottom: 55.3vh;
   color: green;
-  font-size: 1.5rem;
+  font-size: 1rem;
 }
 .item1 {
   height: 5.5vh;
@@ -92,12 +108,40 @@ export default {
   height: 4vh;
   width: 5vh;
   position: absolute;
-  left: .1vh;
+  left: 0.1vh;
   bottom: 32.5vh;
   cursor: pointer;
 }
-.greyed-out{
+.greyed-out {
   opacity: 0.2;
+  cursor: default;
+}
+.currency-btns {
+  display: flex;
+  position: absolute;
+  flex-flow: wrap column;
+  left: 65vh;
+  top: 10vh;
+  justify-content: space-evenly;
+  height: 40vh;
+}
+.getChange {
+  position: absolute;
+  height: 1.5vh;
+  width: 2vh;
+  top: 39.7vh;
+  left: 52vh;
+  cursor: pointer;
+  animation: flash 2s;
+  animation-iteration-count: infinite;
+}
+@keyframes flash {
+  0% {background-color: white}
+  10% {background-color: gold}
+  100% {background-color: gold}
+}
+.gone {
+  display: none;
 }
 </style>
 
